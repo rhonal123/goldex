a22f567be9c93daa8485e379f26bbc369fcc5f4a
rhonal <rhonalchirinos@gmail.com>
2017-07-22 08:08:05 -0400

    base de datos para heroku

--- config/database.php
+++ config/database.php
@@ -76,7 +76,6 @@ return [
             'prefix' => '',
             'schema' => 'public',
         ],
-        /*
         'heroku' => [  
             'driver'   => 'pgsql',
             'host'     => parse_url(getenv("DATABASE_URL"))["host"],
@@ -87,7 +86,6 @@ return [
             'prefix'   => '',
             'schema'   => 'public',
         ],
-        */
     ],
 
     /*
