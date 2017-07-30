3254f4050007b9834dc6d3650873c0d2e9fd42ba
rhonal <rhonalchirinos@gmail.com>
2017-07-30 12:18:07 -0400

    movimientos views migration

---
+++ database/migrations/2017_07_30_161353_movimientoView.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+ 
+class MovimientoView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        DB::statement( '
+        CREATE OR REPLACE VIEW movimiento_views AS 
+        SELECT m.id as id, 
+               m.fecha as fecha,
+               m.referencia as referencia,
+               m.comision as comision,
+               m.monto as monto,
+               m.saldo as saldo,
+               m.descripcion as descripcion,
+               m.cuenta_id as cuenta_id,
+               m.negocio_id as negocio_id,
+               m.clasificacion as clasificacion,
+               c.numero as cuenta,
+               n.nombre as negocio 
+               FROM movimientos m
+         left join cuentas c on m.cuenta_id = c.id
+         left join negocios n on m.negocio_id = n.id;
+        ' );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
